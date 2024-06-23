0e10e57ad3d4939e193be4c6dd8f8746dafbb883
Victor Ross <vicjun22@gmail.com>
2024-06-22 22:57:38 -0300

    Curso Udemy: error handler

--- str-consumer/src/main/java/com/vicjun22/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/vicjun22/str_consumer/listeners/StrConsumerListener.java
@@ -1,18 +1,20 @@
 package com.vicjun22.str_consumer.listeners;
 
 import com.vicjun22.str_consumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION ...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
